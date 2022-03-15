956770c79853f72636ac3724ec530c3584ce6114
VILLENA, Jhazel B <77673298+UBSITVillenaJhazel@users.noreply.github.com>
2022-03-15 23:28:21 +0800

    Update_Review

--- Midterms/appdev-samp/src/app/course-info/course-info.component.ts
+++ Midterms/appdev-samp/src/app/course-info/course-info.component.ts
@@ -1,11 +1,45 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface Student{
+  idNumber: string;
+  name: string;
+  dateAdded: Date;
+  course: string;
+}
+
 @Component({
   selector: 'app-course-info',
   templateUrl: './course-info.component.html',
   styleUrls: ['./course-info.component.css']
 })
 export class CourseInfoComponent implements OnInit {
+
+  students=[{value:'John Uno'},
+            {value:'Ana Nen'},
+            {value:'Zayn Malik'}]
+  students1='Elizabeth';
+
+  student: Student = {
+    idNumber: '20189522',
+    name: 'Jhazel',
+    dateAdded: new Date(2022, 1, 18),
+    course: 'BSIT',
+  };
+
+  student1: Student = {
+    idNumber: '20199522',
+    name: 'Luffy',
+    dateAdded: new Date(2021, 1, 18),
+    course: 'BSCS',
+  };
+
+  student2: Student = {
+    idNumber: '20219522',
+    name: 'Zoro',
+    dateAdded: new Date(2018, 1, 18),
+    course: 'BSCpE',
+  };
+
 
   constructor() { }
 
