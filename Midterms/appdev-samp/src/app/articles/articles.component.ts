5d209eef7858ea2bd89126f007a320f49a0e95bb
VILLENA, Jhazel B <77673298+UBSITVillenaJhazel@users.noreply.github.com>
2022-03-15 12:22:05 +0800

    Template_Syntax

--- Midterms/appdev-samp/src/app/articles/articles.component.ts
+++ Midterms/appdev-samp/src/app/articles/articles.component.ts
@@ -1,11 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface Student{
+  idNumber: string;
+  name: string;
+  dateAdded: Date;
+  course: string;
+}
+
 @Component({
   selector: 'app-articles',
   templateUrl: './articles.component.html',
   styleUrls: ['./articles.component.css']
 })
 export class ArticlesComponent implements OnInit {
+  students=[{value:'John Uno'},
+            {value:'Ana Nen'},
+            {value:'Zayn Malik'}]
+  students1='Elizabeth';
+
+  student: Student = {
+    idNumber: '20189522',
+    name: 'Jhazel',
+    dateAdded: new Date(2022, 1, 18),
+    course: 'BSIT',
+  };
 
   constructor() { }
 
